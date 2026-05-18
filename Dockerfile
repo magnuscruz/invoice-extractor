a8bd230ea3007d69379f8342ee14d0b8e8571c1b
magnuscruz <magnuscruz@gmail.com>
2026-05-18 11:42:53 +0000

    Minor fix CI/CD

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,4 @@ COPY --from=builder /app/dist ./dist
 COPY --from=builder /app/package*.json ./
 COPY --from=builder /app/node_modules ./node_modules
 
-EXPOSE 3000
 CMD ["npm", "start"]
--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Increase limit for base64 images
 app.use(express.json({ limit: '20mb' }));
@@ -92,8 +92,8 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(Number(PORT), "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
